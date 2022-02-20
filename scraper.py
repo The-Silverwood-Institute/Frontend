0415c7addbc7cdd20f72dbf22feeefb437880dc1
Kit <kittsville@googlemail.com>
2022-02-20 21:56:15 +0000

    Add script to download all recipes

---
+++ scraper.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+'''
+Dumps the entire recipe list as a JSON list.
+Useful for parity checking between releases or for data science work
+'''
+
+base_url = "https://api.reciba.se/recipes/"
+
+recipe_list = requests.get(base_url).json()
+
+recipes = []
+
+for recipe_entry in recipe_list:
+    recipe_url = base_url + recipe_entry['url']
+
+    recipes.append(requests.get(recipe_url).json())
+
+with open('recipes.json', 'w') as file:
+    json.dump(recipes, file)
