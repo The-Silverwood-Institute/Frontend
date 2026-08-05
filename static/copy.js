ff762e02e7cb86f4593ef32208c4e4174cd3ca1c
Kit <kittsville@googlemail.com>
2026-08-05 23:21:32 +0100

    Add animation when copying ingredients
    Close #25

--- static/copy.js
+++ static/copy.js
@@ -10,10 +10,15 @@ const getIngredient = ingredientEl => {
 };
 
 document.getElementById('copy').addEventListener('click', e => {
+    const copyEl = e.currentTarget;
     const ingredientEls = Array.from(document.querySelectorAll('.ingredient'));
     const ingredients = ingredientEls.map(getIngredient);
 
     const ingredientList = ingredients.join('\n');
 
-    navigator.clipboard.writeText(ingredientList);
+    navigator.clipboard.writeText(ingredientList).then(() => {
+        copyEl.classList.remove('is-copied');
+        void copyEl.offsetWidth;
+        copyEl.classList.add('is-copied');
+    });
 });
